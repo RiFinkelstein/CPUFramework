8f4314ae63a3a4ed07c50e52f221908774f0f170
rifinkelstein <rivkafinkelstein823@gmail.com>
2025-06-24 11:58:20 -0400

    OOP- CPU Framework

---
+++ bizObject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+//using System.Data.SqlClient;
+using Microsoft.Data.SqlClient; // Ensure this is included
+
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUFramework
+{
+    public class bizObject
+    {
+        string _tablename = ""; string _getsproc = ""; string _updatesproc = ""; string _deletesproc = "";
+        string _primarykeyname = "";
+        string _primarykeyparamname = "";
+        DataTable _datatable = new();
+
+
+
+        public bizObject(string Tablename)
+        {
+            _tablename = Tablename;
+            _getsproc = Tablename+ "Get";
+            _updatesproc = Tablename + "Update";
+            _deletesproc = Tablename + "Delete";
+            _primarykeyname= Tablename+"ID";
+            _primarykeyparamname = "@" + _primarykeyname;
+
+        }
+        public DataTable Load(int primarykeyvalue)
+        {
+            DataTable dt = new();
+            SqlCommand cmd = SQLUtility.GetSqlcommand(_getsproc);
+            SQLUtility.SetParamValue(cmd, _primarykeyparamname, primarykeyvalue);
+            dt = SQLUtility.GetDataTable(cmd);
+            _datatable = dt;
+            return dt;
+        }
+
+        public void Save(DataTable datatable)
+        {
+            if (datatable.Rows.Count == 0)
+            {
+                throw new Exception($"cannot call {_tablename}  save method becasue there are no rows in the table");
+            }
+            DataRow r = datatable.Rows[0];
+            SQLUtility.SaveDataRow(r, _updatesproc);
+        }
+
+
+        public void Delete(DataTable datatable)
+        {
+            int id = (int)datatable.Rows[0][_primarykeyname];
+            SqlCommand cmd = SQLUtility.GetSqlcommand(_deletesproc);
+            SQLUtility.SetParamValue(cmd, _primarykeyparamname, id);
+            SQLUtility.ExecuteSQL(cmd);
+        }
+
+    }
+}
