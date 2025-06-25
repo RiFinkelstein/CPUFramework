a0e8806074f09be297567f592340a836ecb2c2de
rifinkelstein <rivkafinkelstein823@gmail.com>
2025-06-25 12:19:09 -0400

    OOP- CPU Framework

--- bizObject.cs
+++ bizObject.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-//using System.Data.SqlClient;
+﻿//using System.Data.SqlClient;
 using Microsoft.Data.SqlClient; // Ensure this is included
-
-using System.Linq;
+using System.ComponentModel;
+using System.Data;
+using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CPUFramework
 {
-    public class bizObject
+    public class bizObject : INotifyPropertyChanged
     {
         string _tablename = ""; string _getsproc = ""; string _updatesproc = ""; string _deletesproc = "";
         string _primarykeyname = "";
         string _primarykeyparamname = "";
         DataTable _datatable = new();
+        List<PropertyInfo> _properties = new();
 
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-
-        public bizObject(string Tablename)
+        public bizObject()
         {
-            _tablename = Tablename;
-            _getsproc = Tablename+ "Get";
-            _updatesproc = Tablename + "Update";
-            _deletesproc = Tablename + "Delete";
-            _primarykeyname= Tablename+"ID";
+            Type t = this.GetType();
+            _tablename = t.Name;
+            if (_tablename.ToLower().StartsWith("biz")) { _tablename = _tablename.Substring(3); }
+            _getsproc = _tablename + "Get";
+            _updatesproc = _tablename + "Update";
+            _deletesproc = _tablename + "Delete";
+            _primarykeyname = _tablename + "ID";
             _primarykeyparamname = "@" + _primarykeyname;
+            _properties = t.GetProperties().ToList<PropertyInfo>();
 
         }
         public DataTable Load(int primarykeyvalue)
@@ -36,8 +36,25 @@ namespace CPUFramework
             SqlCommand cmd = SQLUtility.GetSqlcommand(_getsproc);
             SQLUtility.SetParamValue(cmd, _primarykeyparamname, primarykeyvalue);
             dt = SQLUtility.GetDataTable(cmd);
+            if (dt.Rows.Count > 0)
+            {
+                LoadProps(dt.Rows[0]);
+            }
             _datatable = dt;
             return dt;
+        }
+        private void LoadProps(DataRow dr)
+        {
+            foreach (DataColumn col in dr.Table.Columns)
+            {
+
+                string colname = col.ColumnName.ToLower();
+                PropertyInfo? prop = _properties.FirstOrDefault(p => p.Name.ToLower() == colname && p.CanWrite == true);
+                if (prop != null)
+                {
+                    prop.SetValue(this, dr[colname]);
+                }
+            }
         }
 
         public void Save(DataTable datatable)
@@ -50,6 +67,34 @@ namespace CPUFramework
             SQLUtility.SaveDataRow(r, _updatesproc);
         }
 
+        public void Save()
+        {
+            SqlCommand cmd = SQLUtility.GetSqlcommand(_updatesproc);
+            foreach (SqlParameter param in cmd.Parameters)
+            {
+                string colname = param.ParameterName.ToLower().Substring(1);
+                PropertyInfo? prop = _properties.FirstOrDefault(p => p.Name.ToLower() == colname && p.CanRead == true);
+                if (prop != null)
+                {
+                    param.Value= prop.GetValue(this);
+                }
+            }
+            SQLUtility.ExecuteSQL(cmd);
+            foreach(SqlParameter param in cmd.Parameters)
+            {
+                if(param.Direction== ParameterDirection.InputOutput)
+                {
+                    string colname = param.ParameterName.ToLower().Substring(1);
+                    PropertyInfo? prop = _properties.FirstOrDefault(p => p.Name.ToLower() == colname && p.CanRead == true);
+                    if (prop != null)
+                    {
+                       prop.SetValue(this, param.Value);
+                    }
+                }
+            }
+        }
+
+
 
         public void Delete(DataTable datatable)
         {
@@ -57,6 +102,10 @@ namespace CPUFramework
             SqlCommand cmd = SQLUtility.GetSqlcommand(_deletesproc);
             SQLUtility.SetParamValue(cmd, _primarykeyparamname, id);
             SQLUtility.ExecuteSQL(cmd);
+        }
+        protected void InvokePropertyChanged([CallerMemberName] string propertyname = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
         }
 
     }
